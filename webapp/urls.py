b9eb7cb559803c1469f6198857d1c608004c0e8a
Joshua Frank Rayo <kean.franco07@gmail.com>
2015-11-11 00:57:04 +0800

    created logo, with gimp format. finished login page w/o auth

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^create/$', views.create, name='create')
+    url(r'^create/$', views.create, name='create'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^dashboard/$', views.dashboard, name='dashboard')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -4,9 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {'pagename': 'Welcome'}
-    return render(request, 'webapp/home.html', context)
+    return render(request, 'home/home.html', context)
+
+
+def login(request):
+    context = {'pagename': 'Login'}
+    return render(request, 'home/login.html', context)
+
+
+def dashboard(request):
+    context = {'pagename': 'Home'}
+    return render(request, 'dashboard/dashboard.html', context)
 
 
 def create(request):
     context = {'pagename': 'Create Scrapbook'}
-    return render(request, 'webapp/create.html', context)
+    return render(request, 'dashboard/create.html', context)
